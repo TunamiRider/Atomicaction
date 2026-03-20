369914a1ac7cf9b377d67330adf85fed805cb69f
TunamiRider <yoite.sy@gmail.com>
2026-03-20 16:51:43 -0700

    Initial Commit

---
+++ AtomicactionTests/AtomicactionTests.swift
@@ -0,0 +1,16 @@
+//
+//  AtomicactionTests.swift
+//  AtomicactionTests
+//
+//  Created by Yuki Suzuki on 3/20/26.
+//
+
+import Testing
+
+struct AtomicactionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
